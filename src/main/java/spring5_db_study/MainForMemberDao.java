b65f0f107f3138a65f5abbb2d91e7070e4b840db
mskim <net94@naver.com>
2021-05-18 09:09:10 +0900

    aa

--- src/main/java/spring5_db_study/MainForMemberDao.java
+++ src/main/java/spring5_db_study/MainForMemberDao.java
@@ -1,8 +1,9 @@
 package spring5_db_study;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import spring5_db_study.config.AppCtx;
@@ -11,14 +12,24 @@ import spring5_db_study.spring.MemberDao;
 
 public class MainForMemberDao {
 	private static MemberDao memberDao;
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMddHHmmss");
 
 	public static void main(String[] args) {
         try(AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);){
-            DataSource ds = ctx.getBean(DataSource.class);
-            System.out.println(ds);
-            
+//            DataSource ds = ctx.getBean(DataSource.class);
+//            System.out.println(ds);
+//            
             memberDao = ctx.getBean(MemberDao.class);
             selectAll();
+            memberDao.insert(new Member("test@test.co.kr", "1234", "test", LocalDateTime.now()));
+            selectAll();
+            insertMember();
+            selectAll();
+            updateMember();
+            selectAll();
+            deleteMember();
+            selectAll();
+
         }
 	}
 	
@@ -33,5 +44,30 @@ public class MainForMemberDao {
         }
     }
 
+    private static void insertMember() {
+        System.out.println("----- insertMember");
+        String prefix = formatter.format(LocalDateTime.now());
+        Member member = new Member(prefix + "@test.co.kr", prefix, prefix, LocalDateTime.now());
+        memberDao.insert(member);
+        System.out.println(member.getId() + " 데이터 추가");
+    }
+
+    private static void updateMember() {
+        System.out.println("----- updateMember");
+        Member member = memberDao.selectByEmail("test@test.co.kr");
+        String oldPw = member.getPassword();
+        String newPw = Double.toHexString(Math.random());
+        member.changePassword(oldPw, newPw);
+
+        memberDao.update(member);
+        System.out.println("암호 변경 : " + oldPw + " > " + newPw);
+        System.out.println(member.getId() + " 데이터 삭제");
+    }
+
+    private static void deleteMember() {
+        System.out.println("----- deleteMember");
+        Member member = memberDao.selectByEmail("test@test.co.kr");
+        memberDao.delete(member);
+    }
 
 }
--- src/main/java/spring5_db_study/spring/MemberDao.java
+++ src/main/java/spring5_db_study/spring/MemberDao.java
@@ -1,10 +1,17 @@
 package spring5_db_study.spring;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -22,9 +29,6 @@ public class MemberDao {
         return results.isEmpty()?null:results.get(0);
     }
 
-    public void insert(Member member) {}
-    public void update(Member member) {}
-
     public List<Member> selectAll() {
         return jdbcTemplate.query("select * from member", new MemberRowMapper());
     }
@@ -34,4 +38,40 @@ public class MemberDao {
         return jdbcTemplate.queryForObject("select count(*) from member", Integer.class);
     }
 
+    public void insert(Member member) {
+        PreparedStatementCreator psc = new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement pstmt = con.prepareStatement("insert into member(email, password, name, regdate) values(?, ?, ?, ?)", new String[] {"id"});
+                pstmt.setString(1, member.getEmail());
+                pstmt.setString(2, member.getPassword());
+                pstmt.setString(3, member.getName());
+                pstmt.setTimestamp(4, Timestamp.valueOf(member.getRegisterDateTime()));
+                return pstmt;
+            }
+        };
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(psc, keyHolder);
+        
+        Number keyValue = keyHolder.getKey();
+        member.setId(keyValue.longValue());
+    }
+    
+    public void update(Member member) {
+    	jdbcTemplate.update("update member set name=?, password=? where email=?", member.getName(), member.getPassword(), member.getEmail());
+    }
+    
+    public void delete(Member member) {
+        PreparedStatementCreator psc = new PreparedStatementCreator() {
+        @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement pstmt = con.prepareStatement("delete from member where email=?");
+                pstmt.setString(1, member.getEmail());
+                return pstmt;
+            }
+        };
+        jdbcTemplate.update(psc);
+
+    }
 }
